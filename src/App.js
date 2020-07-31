48de9c39e14f7b2aafc3a8600ce5e8dc778b5c86
Tiffany <hainguyen17@augustana.edu>
2020-08-01 02:31:53 +0700

    OT cuc manh to add two more screens

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Redirect, Switch, Route } from 'react-router-dom';
 import './App.css';
 import MainScreen from './views/MainScreen';
 import FaceScreen from './views/FaceScreen';
+import IdScreen from './views/IdScreen';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={MainScreen} />
         <Route exact path="/face-screen" component={FaceScreen} />
-        <Route exact path="/face-confirm" component={FaceScreen} />
+        <Route exact path="/id-screen" component={IdScreen} />
         <Redirect to="/" />
       </Switch>
     </div>
---
+++ src/views/Common/Webcam.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+import Button from './Button';
+import { useHistory } from 'react-router-dom';
+
+const WebcamCapture = () => {
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = React.useState(null);
+  const history = useHistory();
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  };
+
+  const handleNext = () => {
+    history.push('/id-screen');
+  };
+
+  const handleCapture = () => {
+    setImgSrc(null);
+  };
+
+  return (
+    <>
+      {imgSrc ? (
+        <>
+          <img src={imgSrc} />
+          <Button onClick={handleCapture} title="CHỤP LẠI" />
+          <Button onClick={handleNext} title="TIẾP" />
+        </>
+      ) : (
+        <>
+          <Webcam
+            screenshotQuality={1}
+            mirrored
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+          />
+          <Button onClick={capture} title="CHỤP ẢNH" />
+        </>
+      )}
+    </>
+  );
+};
+// https://www.npmjs.com/package/react-webcam
+
+export default WebcamCapture;
